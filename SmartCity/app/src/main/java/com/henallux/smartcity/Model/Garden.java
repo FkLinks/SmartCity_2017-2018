837df40f86d054c444b86e9b21b4cce8c2a8fc82
QuentinVaneck <quentin.vaneck@gmail.com>
2017-11-22 12:35:30 +0100

    creation dao

---
+++ SmartCity/app/src/main/java/com/henallux/smartcity/Model/Garden.java
@@ -0,0 +1,71 @@
+package com.henallux.smartcity.Model;
+
+public class Garden {
+    private String name;
+    private double surfaceArea;
+    private String street;
+    private int numStreet;
+    private String description;
+    private double note;
+    private String geographicalCoordinates;
+
+    public Garden(String name, double surfaceArea, String street, int numStreet, String description, double note,String geographicalCoordinates) {
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSurfaceArea() {
+        return surfaceArea;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public int getNumStreet() {
+        return numStreet;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getNote() {
+        return note;
+    }
+
+    public String getGeographicalCoordinates() {
+        return geographicalCoordinates;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurfaceArea(double surfaceArea) {
+        this.surfaceArea = surfaceArea;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setNumStreet(int numStreet) {
+        this.numStreet = numStreet;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setNote(double note) {
+        this.note = note;
+    }
+
+    public void setGeographicalCoordinates(String geographicalCoordinates) {
+        this.geographicalCoordinates = geographicalCoordinates;
+    }
+}
